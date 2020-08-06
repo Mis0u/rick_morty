99c5f5f9f18d2523d595c3a888c5d634d14fbcbc
Misou <micka75bup@gmail.com>
2020-08-06 21:35:32 +0200

    Affichage du loader

--- assets/js/ajax-location-episode.js
+++ assets/js/ajax-location-episode.js
@@ -1,15 +1,19 @@
 async function main() {
-    let residents = document.querySelectorAll('.js-actual-residents')
+  let residents = document.querySelectorAll(".js-actual-residents");
+  let load = document.querySelector("#load");
 
-    for (let i = 0; i < residents.length; i++) {
-        let resident = await fetch(residents[i].dataset.character)
-            .then(reponse => reponse.json())
-            .then(reponseJson => reponseJson)
-        residents[i].setAttribute("src", resident.image)
-        residents[i].parentElement.setAttribute('title', resident.name)
-        residents[i].previousSibling.previousSibling.setAttribute('href', "/characters?name=" + resident.name)
-    }
+  for (let i = 0; i < residents.length; i++) {
+    let resident = await fetch(residents[i].dataset.character)
+      .then((reponse) => reponse.json())
+      .then((reponseJson) => reponseJson);
 
+    residents[i].setAttribute("src", resident.image);
+    residents[i].parentElement.setAttribute("title", resident.name);
+    residents[i].previousSibling.previousSibling.setAttribute(
+      "href",
+      "/characters?name=" + resident.name
+    );
+  }
 }
 
-main()
+main();
